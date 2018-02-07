3765315e21d11cff698ed5bf7ab12b0af81618d8
gugolya <gugolya@gmail.com>
2018-02-07 22:06:21 +0100

    Ora01 anyaga
    Ora01 anyaga: fényképezés

---
+++ Ora01_MakePicture/translations/translation_en_US.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hey</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hey</translation>
+        </message>
+    </context>
+</TS>
